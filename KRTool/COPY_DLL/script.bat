d7b1c44e544f185f8dca93414d150bc4e61e622f
karikera <karikera3@hotmail.com>
2020-05-08 17:49:58 +0900

    libmariadb from internal

--- KRTool/COPY_DLL/script.bat
+++ KRTool/COPY_DLL/script.bat
@@ -23,7 +23,7 @@ kcopy "%thirddir%\ken-res-loader\lib\%platform%\libpng16%d%.dll" "%outdir%libpng
 kcopy "%thirddir%\ken-res-loader\lib\%platform%\libvorbis%d%.dll" "%outdir%libvorbis%d%.dll"
 kcopy "%thirddir%\ken-res-loader\lib\%platform%\libvorbisfile%d%.dll" "%outdir%libvorbisfile%d%.dll"
 kcopy "%thirddir%\ken-res-loader\lib\%platform%\jpeg%d%.dll" "%outdir%jpeg%d%.dll"
-kcopy "%MYSQL_C_DIR%\lib\libmariadb.dll" "%outdir%libmariadb.dll"
+kcopy "%thirddir%\mariadb\lib\%platform%\%configdir%\libmariadb.dll" "%outdir%libmariadb.dll"
 kcopy "%thirddir%\fftw-3.3.5-dll-%platform%\libfftw3f-3.dll" "%OutDir%libfftw3f-3.dll"
 kcopy "%thirddir%\angle\lib\%platform%\%configdir%\libEGL.dll" "%OutDir%libEGL.dll"
 kcopy "%thirddir%\angle\lib\%platform%\%configdir%\libGLESv2.dll" "%OutDir%libGLESv2.dll"
